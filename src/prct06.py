3d8e7995e3f1afeb2a6ffe689a77cbb39bd30671
ivan hernandez <alu0100845013@student07.aulas.priv>
2014-03-14 09:11:53 +0000

    aproximacion

---
+++ src/prct06.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+PI=3.14159265358979323846264338327950288
+def aproximacion(n):
+ if (n!=0):
+    s=0.0
+    for i in range(1,n+1):
+     a= float(i-1)/n
+     b= float (i)/n
+     xi= (i-0.5)/n
+     fxi= 4.0/(1.0 + xi*xi)
+     s+=fxi
+    r=s/n
+    return r
+
+n=int(raw_input(' valor de n: '))
+m=int(raw_input(' valor de m: '))
+lista=[]
+print aproximacion(n)
+for j in range(1,m+1):
+  pi=aproximacion(j*m)
+  lista= lista +[pi]
+print lista
